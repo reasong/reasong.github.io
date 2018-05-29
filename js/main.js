10000f0011b07e3ddd91f38e9355230191b13a8d
a_z <a.flyde@gmail.com>
2018-05-30 01:21:19 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,3 +13,93 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
+
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+    if ($('#begin').isInViewport()) {
+        begin.style.opacity = 1;
+        begin.style.transform = "translateY(0)";
+        begin.style.transform = "skewY(0deg)";
+    } else {
+          begin.style.opacity = 0;
+          begin.style.transform = "translateY(200px)";
+          begin.style.transform = "skewY(-4.5deg)";
+        }
+});
+
+$(window).on('resize scroll', function() {
+    if ($('#target').isInViewport()) {
+        target.style.opacity = 1;
+        target.style.transform = "translateY(0)";
+        target.style.transform = "skewY(0deg)";
+    } else {
+          target.style.opacity = 0;
+          target.style.transform = "translateY(200px)";
+          target.style.transform = "skewY(-4.5deg)";
+        }
+});
+
+$(window).on('resize scroll', function() {
+    if ($('#arrows').isInViewport()) {
+        arrows.style.opacity = 1;
+        arrows.style.transform = "translateY(0)";
+        arrows.style.transform = "skewY(0deg)";
+    } else {
+          arrows.style.opacity = 0;
+          arrows.style.transform = "translateY(-200px)";
+          arrows.style.transform = "skewY(-4.5deg)";
+        }
+});
+
+$(window).on('resize scroll', function() {
+    if ($('#salons').isInViewport()) {
+        salons.style.opacity = 1;
+        salons.style.transform = "translateY(0)";
+        salons.style.transform = "skewY(0deg)";
+    } else {
+          salons.style.opacity = 0;
+          salons.style.transform = "translateY(300px)";
+          salons.style.transform = "skewY(-4.5deg)";
+        }
+});
+
+$(window).on('resize scroll', function() {
+    if ($('#faq').isInViewport()) {
+        faq.style.opacity = 1;
+        faq.style.transform = "translateY(0)";
+        faq.style.transform = "skewY(0deg)";
+    } else {
+          faq.style.opacity = 0;
+          faq.style.transform = "translateY(100px)";
+          faq.style.transform = "skewY(-4.5deg)";
+        }
+});
+
+$(window).on('resize scroll', function() {
+    if ($('#send').isInViewport()) {
+      send.style.opacity = 1;
+      send.style.transform = "translateY(0)";
+      send.style.transform = "skewY(0deg)";
+    } else {
+      send.style.opacity = 0;
+      send.style.transform = "translateY(200px)";
+      send.style.transform = "skewY(-4.5deg)";
+    }
+});
